467d8e3fee4fa15ec626627c301059b9441e33bd
finneh4249 <ethancornwill@outlook.com>
2024-09-08 20:52:12 +1000

    * docs(init.py): added module docstrings * feat(main.py): added app creation function * docs(user.py): added model and schema docstrings

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
+"""Main application initialization and configuration.
+
+Contains the code that creates and configures the Flask application.
+"""
+
 import os
-from init import db, ma, bcrypt, jwt
 
 from flask import Flask
 
-def create_app():
+from init import db, ma, bcrypt, jwt
 
+
+def create_app():
+    """Create and configure an instance of the Flask application.
+
+    Returns
+    -------
+    Flask
+        The configured Flask application.
+    """
     app = Flask(__name__)
+
     app.json.sort_keys = False
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
-
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    
-
     return app
