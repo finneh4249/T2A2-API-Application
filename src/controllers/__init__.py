1d0c7ca1abd466a16912645b1d6fc3364d389480
finneh4249 <ethancornwill@outlook.com>
2024-09-27 00:59:16 -0700

    refactor: Update controllers module with a module docstring

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,19 @@
+"""
+This module contains all the controllers for the application.
+
+The controllers are:
+
+- `auth_controller`: Handles authentication and authorization.
+- `user_controller`: Handles user-related operations.
+- `cli_controller`: Handles CLI-related operations.
+- `post_controller`: Handles post-related operations.
+- `comment_controller`: Handles comment-related operations.
+- `like_controller`: Handles like-related operations.
+- `feed_controller`: Handles feed-related operations.
+- `follow_controller`: Handles follow-related operations.
+
+"""
+
 from .auth_controller import auth_controller as auth
 from .user_controller import user_controller as user
 from .cli_controller import cli_controller as cli
